06e70d8d02ac30c2800621b88fd010598d193d93
Jake Causon <jake.causon@gmail.com>
2015-08-30 19:37:39 +0100

    Move examples to own directory

---
+++ examples/basic.js
@@ -0,0 +1,10 @@
+var api = require('./lib/ApiQuick');
+api.init(8080);
+api.addPackage('date', 
+	{
+		'now': function(method, arg, params) {
+			var currentDate = new Date();
+			return {time:currentDate.toUTCString()};
+		}
+	}
+);
